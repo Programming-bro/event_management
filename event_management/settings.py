571f89ea9211d0fa3b272e5f3b40fdbb387aa587
Pabon <fahimsadik933@gmail.com>
2026-02-02 22:04:16 +0600

    csrf token added

--- event_management/settings.py
+++ event_management/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGIN = ['https://*.onrender.com','http://127.0.0.1:8000']
+CSRF_TRUSTED_ORIGINS = ['https://*.onrender.com','http://127.0.0.1:8000']
 
 # Application definition
 
