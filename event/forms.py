333388d792411535313048d7b1c91a82fb8be963
Pabon <fahimsadik933@gmail.com>
2026-02-17 21:55:02 +0600

    user authentication system added

--- event/forms.py
+++ event/forms.py
@@ -1,26 +1,13 @@
 from django import forms
-from event.models import Event, Participant, Category
+from event.models import Event, Category
 
-#Django form
-
-# class TaskForm(forms.Form):
-#     title = forms.CharField(max_length=250, label="Task Title")
-#     description = forms.CharField(widget=forms.Textarea, label="Task Description")
-#     due_date = forms.DateField(widget=forms.SelectDateWidget, label="Due Date")
-#     assigned_to = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple,choices=[])
-
-#     def __init__(self,*args,**kwargs):
-#         # print(args,kwargs)
-#         employees = kwargs.pop("employees",[])
-#         super().__init__(*args,**kwargs)
-#         self.fields['assigned_to'].choices = [(emp.id,emp.name) for emp in employees]
 
 #Django model form 
 
 class EventModelForm(forms.ModelForm):
     class Meta:
         model = Event
-        fields = ['name','description','event_date','attended','category','location']
+        fields = ['name','description','event_date','participant','category','location','asset']
 
         widgets = {
             'name' : forms.TextInput(attrs={
@@ -38,5 +25,43 @@ class EventModelForm(forms.ModelForm):
                 'class': "border-2 border-gray-500 w-100 rounded-lg mt-5 px-2",
                 'placeholder': "Event Location"
             }),
-            'attended' : forms.CheckboxSelectMultiple,
+            'participant' : forms.CheckboxSelectMultiple,
         }
+
+class StyledFormMixin:
+    """ Mixing to apply style to form field"""
+
+    def __init__(self, *arg, **kwarg):
+        super().__init__(*arg, **kwarg)
+        self.apply_styled_widgets()
+
+    default_classes = "border-2 border-gray-300 w-full p-3 rounded-lg shadow-sm focus:outline-none focus:border-rose-500 focus:ring-rose-500"
+
+    def apply_styled_widgets(self):
+        for field_name, field in self.fields.items():
+            if isinstance(field.widget, forms.TextInput):
+                field.widget.attrs.update({
+                    'class': self.default_classes,
+                    'placeholder': f"Enter {field.label.lower()}"
+                })
+            elif isinstance(field.widget, forms.Textarea):
+                field.widget.attrs.update({
+                    'class': f"{self.default_classes} resize-none",
+                    'placeholder':  f"Enter {field.label.lower()}",
+                    'rows': 5
+                })
+            elif isinstance(field.widget, forms.SelectDateWidget):
+                print("Inside Date")
+                field.widget.attrs.update({
+                    "class": "border-2 border-gray-300 p-3 rounded-lg shadow-sm focus:outline-none focus:border-rose-500 focus:ring-rose-500"
+                })
+            elif isinstance(field.widget, forms.CheckboxSelectMultiple):
+                print("Inside checkbox")
+                field.widget.attrs.update({
+                    'class': "space-y-2"
+                })
+            else:
+                print("Inside else")
+                field.widget.attrs.update({
+                    'class': self.default_classes
+                })
--- event/urls.py
+++ event/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
-from event.views import mew, test, home, event_details, create_event, org_dash, update_event, delete_event
+from event.views import events, event_details, create_event, org_dash, update_event, delete_event, book_event, user_dash, dashboard
 
 urlpatterns = [
-    path('mew/',mew),
-    path('test/',test),
-    path('home/',home,name="home"),
+    path('events/',events,name="events"),
+    path('dashboard', dashboard, name='dashboard'),
     path('event_details/<int:id>/',event_details, name="event_details"),
     path('create_event/',create_event,name="create_event"),
     path('organizer_dashboard/',org_dash,name="organizer_dashboard"),
+    path('user_dashboard/',user_dash,name="user_dashboard"),
     path('update_event/<int:id>/',update_event, name="update_event"),
     path('delete_event/<int:id>/',delete_event, name="delete_event"),
+    path('book_event/<int:id>/',book_event,name="book_event")
 ]
