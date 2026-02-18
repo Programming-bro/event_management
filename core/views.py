8fbe210865c5e54ff29136a49546a6f2f04d43f5
Pabon <fahimsadik933@gmail.com>
2026-02-18 13:20:09 +0600

    env variable updated

--- core/views.py
+++ core/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    group =  request.user.groups.first().name
+    return render(request, 'home.html', {"group":group})
 
 def no_permission(request):
     return render(request, 'no_permission.html')
--- event_management/settings.py
+++ event_management/settings.py
@@ -1,8 +1,14 @@
 from pathlib import Path
 import dj_database_url 
 from decouple import config
+import os
+import environ
 
 
+env = environ.Env(
+    DEBUG=(bool, True), # set default value and cast to bool
+    DATABASE_URL=(str, 'postgresql://event_db_zmkk_user:Zy4V99f270RhY6WNMZ26tKxBBJYGJsST@dpg-d609uu4hg0os73a6dtlg-a.oregon-postgres.render.com/event_db_zmkk') # example default for DB
+)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -12,7 +18,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -150,14 +157,17 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
-EMAIL_PORT = config('EMAIL_PORT', cast=int)
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+
+
+EMAIL_BACKEND = env('EMAIL_BACKEND')
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_USE_TLS = env('EMAIL_USE_TLS', cast=bool)
+EMAIL_PORT = env('EMAIL_PORT', cast=int)
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 
 FRONTEND_URL = 'http://127.0.0.1:8000'
 
