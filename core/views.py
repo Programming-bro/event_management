b4b7cf512ac858edca38b14bbae1ce069522a975
Pabon <fahimsadik933@gmail.com>
2026-02-19 00:20:40 +0600

    home page bug fixed

--- core/views.py
+++ core/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    group =  request.user.groups.first().name
+    group = ""
+    if(request.user.is_authenticated):
+        group =  request.user.groups.first().name
     return render(request, 'home.html', {"group":group})
 
 def no_permission(request):
