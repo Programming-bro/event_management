29a8e8ed0019c391c4df50a85c26ea77e9fd5d7b
Pabon <fahimsadik933@gmail.com>
2026-02-23 03:58:56 +0600

    profile system updated

--- event_management/settings.py
+++ event_management/settings.py
@@ -87,24 +87,24 @@ WSGI_APPLICATION = 'event_management.wsgi.application'
 # }
 
 #for postgre
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         # Replace this value with your local database's connection string.
-#         default='postgresql://event_db_zmkk_user:Zy4V99f270RhY6WNMZ26tKxBBJYGJsST@dpg-d609uu4hg0os73a6dtlg-a.oregon-postgres.render.com/event_db_zmkk',
-#         conn_max_age=600
-#     )
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'event_m',
-        'USER': 'postgres',
-        'PASSWORD': '2410',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://event_db_2_user:9bGqOx2wr3b8cnRuXn1y04Rj3hDYPrdt@dpg-d6dn9esr85hc73c17bv0-a.oregon-postgres.render.com/event_db_2',
+        conn_max_age=600
+    )
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'event_m',
+#         'USER': 'postgres',
+#         'PASSWORD': '2410',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# }
 
 
 # Password validation
